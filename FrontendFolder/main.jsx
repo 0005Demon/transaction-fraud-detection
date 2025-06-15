01af9ecd086a69b2277b240384e52b7456fcfbde
0005Demon <97457580+0005Demon@users.noreply.github.com>
2025-06-15 09:30:51 +0530

    Update main.jsx

--- FrontendFolder/main.jsx
+++ FrontendFolder/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import InvoiceFraudDetector from './App'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <InvoiceFraudDetector />
+  </React.StrictMode>
+)
